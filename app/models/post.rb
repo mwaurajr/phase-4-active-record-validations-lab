7f3c4098372c2f49d9af82eab979ffc89ca5d62a
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-11-24 20:39:46 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
-end
+    # valiadate user presence
+    validates :title, presence: true  
+    # ensures content's minimum length  250 
+    validates :content, length: { minimum: 250 }
+    # ensures summary's maximum length  250 
+    validates :summary, length: { maximum: 250 }
+    # "Inclusion" validates that the attributes' values are included in a given se
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+  
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+  end
